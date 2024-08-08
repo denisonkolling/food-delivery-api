20202625103c67ec20587e54ce5b2ac8f2b00566
Denison Kolling <denisonkolling@gmail.com>
2024-08-08 14:42:11 -0300

    feat: add create user method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+
+import {
+  EntityManager,
+  UniqueConstraintViolationException,
+} from '@mikro-orm/postgresql';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly entityManager: EntityManager) { }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const existingUser = await this.entityManager.findOne(User, {
+        email: createUserDto.email,
+      });
+
+      if (existingUser) {
+        throw new UniqueConstraintViolationException(
+          new Error(`An user with email ${createUserDto.email}already exists.`),
+        );
+      }
+
+      const user = new User();
+      this.entityManager.assign(user, createUserDto);
+      await this.entityManager.persistAndFlush(user);
+
+      return user;
+
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'An error occurred while saving user data',
+      );
+    }
   }
 }
