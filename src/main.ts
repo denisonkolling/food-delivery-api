9411dc847f2222ebedeb44e079ad878c6a6a4d48
Denison Kolling <denisonkolling@gmail.com>
2024-08-12 17:32:46 -0300

    feat: add all exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UniqueConstraintExceptionFilter } from './common/filters/unique-constraint-conflict-exception.filter';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './common/filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new UniqueConstraintExceptionFilter());
+  app.useGlobalFilters(new UniqueConstraintExceptionFilter(), new AllExceptionsFilter());
   const config = new DocumentBuilder()
     .setTitle('Food Delivery')
     .setDescription('The food delivery API application')
