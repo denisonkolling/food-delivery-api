4f6e56516aa9bff596caab77ad5434f181f6a19c
Denison Kolling <denisonkolling@gmail.com>
2024-09-23 17:36:21 -0300

    refactor: Capitalize strings in OrderStatus enum for consistency

--- src/order/enums/order-status.enum.ts
+++ src/order/enums/order-status.enum.ts
@@ -1,6 +1,6 @@
 export enum OrderStatus {
-    PENDING = 'pending',
-    PROCESSING = 'processing',
-    COMPLETED = 'completed',
-    CANCELLED = 'cancelled',
+    PENDING = 'PENDING',
+    PROCESSING = 'PROCESSING',
+    COMPLETED = 'COMPLETED',
+    CANCELLED = 'CANCELLED',
 }
